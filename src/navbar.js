19391e2a53fc310766b82c0f9fd3dc75bede4ee7
Gian <e.dolphinxyz@gmail.com>
2022-08-12 20:22:02 -0500

    Switch to react-bootstrap

--- src/navbar.js
+++ src/navbar.js
@@ -1,37 +1,28 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function NavBar() {
     return (
       <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">BadBank</a>
-          <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon'></span>
-          </button>
-          <div className='collapse navbar-collapse' id='navbarNav'>
-            <ul className='navbar-nav'>
-              <li className='nav-item'>
-                <a className='nav-link' href='#/CreateAccount/'>Create Account</a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='#/login/'>Login</a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='#/deposit/'>Deposit</a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='#/withdraw/'>Withdraw</a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='#/balance/'>Balance</a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='#/alldata/'>AllData</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#">BadBank</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#/CreateAccount/">Create Account</Nav.Link>
+                <Nav.Link href='#/login/'>Login</Nav.Link>
+                <Nav.Link href='#/deposit/'>Deposit</Nav.Link>
+                <Nav.Link href='#/withdraw/'>Withdraw</Nav.Link>
+                <Nav.Link href='#/balance/'>Balance</Nav.Link>
+                <Nav.Link href='#/alldata/'>AllData</Nav.Link>          
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </>
     );
 }
