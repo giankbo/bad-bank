451c400d5a28533ebb7d9784325570e3c07603ba
Gian <e.dolphinxyz@gmail.com>
2022-08-15 18:03:46 -0500

    Update code

--- src/login.js
+++ src/login.js
@@ -1,11 +1,63 @@
 import React from "react";
 import { UserContext } from "./context";
+import Card from "./context";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';  
 
 export default function Login() {
-    const ctx = React.useContext(UserContext)
+    const {useState, useContext} = React;
+    const [show, setShow] = useState(true);
+    const [status, setStatus] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const ctx = useContext(UserContext);
+
+    function validate(field, label) {
+        if(!field) {
+            setStatus(`Error: ${label}`);
+            setTimeout(() => setStatus(''), 3000);
+            return false;
+        }
+        return true;
+    }
+
+    function handleCreate() {
+        console.log(email, password);
+        if(!validate(email, 'email')) return;
+        if(!validate(password, 'password')) return;
+        ctx.users.push({email, password, balance:100});
+        setShow(false);
+    }
+
+    function clearForm() {
+        setEmail('');
+        setPassword('');
+        setShow(true)
+    }
+
     return (
-        <h1>Login <br/>
-            {JSON.stringify(ctx)}
-        </h1>
+        <Card 
+        bgcolor = 'light'
+        txtcolor = "dark"
+        header = 'Login'
+        status = {status}
+        body = {show ? (
+                    <>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="input" placeholder="Enter email" id="email" value={email} onChange={e => setEmail(e.currentTarget.value)} />
+                    <br />
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter password" id="password" value={password} onChange={e => setPassword(e.currentTarget.value)} />
+                    <br />
+                    <Button variant="primary" type="submit" onClick={handleCreate}>Login</Button>
+                    </>
+                ):(
+                    <>
+                    <h5>Success</h5>
+                    <Button variant="primary" type="submit" onClick={clearForm}>Return to login</Button>
+                    </>
+                )}
+        />
     );
 }
