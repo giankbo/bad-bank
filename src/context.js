8c57819e89dfeafe6bc27fad49aea05eeadb49e1
Gian <e.dolphinxyz@gmail.com>
2022-08-15 17:54:13 -0500

    Add template literals

--- src/context.js
+++ src/context.js
@@ -4,9 +4,9 @@ export const UserContext = React.createContext(null);
 
 export default function Card(props) {
     function classes() {
-        const bg = props.bgcolor ? ' bg-' + props.bgcolor : ' ';
-        const txt = props.txtcolor ? ' text-' + props.txtcolor : ' text-white';
-        return 'card mb-3 ' + bg + txt;
+        const bg = props.bgcolor ? `bg-${props.bgcolor}` : ' ';
+        const txt = props.txtcolor ? `text-${props.txtcolor}` : ' text-white';
+        return  `card mb-3 ${bg} ${txt}`;
     }
 
     return(
