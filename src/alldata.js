8672599a2903f60d64cc791a7687ddb6e66d6587
Gian <e.dolphinxyz@gmail.com>
2022-09-04 13:55:02 -0500

    Add Bootstrap’s card

--- src/alldata.js
+++ src/alldata.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { UserContext } from "./context";
+import Card from "./context";
 
 export default function AllData() {
     const ctx = React.useContext(UserContext)
     return (
-        <h1>All Data<br/>
+        <Card
+        bgcolor = 'light'
+        txtcolor = "dark"
+        header = 'AllData'
+        body={
+        <ul>
             {JSON.stringify(ctx)}
-        </h1>
+        </ul>
+        }
+        />
     );
 }
