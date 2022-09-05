b726e1d29e233f280eb64b3b1c438970afc4e882
Gian <e.dolphinxyz@gmail.com>
2022-09-05 12:11:55 -0500

    Add logic, react-bootstrap and sweetalert2

--- src/deposit.js
+++ src/deposit.js
@@ -1,11 +1,89 @@
 import React from "react";
 import { UserContext } from "./context";
-
+import Card from "./context";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+ 
 export default function Deposit() {
-    const ctx = React.useContext(UserContext)
+    const {useState, useContext} = React
+    const ctx = useContext(UserContext);
+    const [deposit, setDeposit] = useState(0);
+    const MySwal = withReactContent(Swal)
+    
+    const handleDeposit = () => {
+      if (ctx.loggedIn.status === false) {
+        MySwal.fire({
+            title: <strong>You got an error!</strong>,
+            html: `Please you need to login`,
+            icon: "error",
+            confirmButtonColor: '#007bff'
+        });
+        return false;
+      }
+
+      if (isNaN(deposit)) {
+        setDeposit(0);
+        MySwal.fire({
+            title: <strong>You got an error!</strong>,
+            html: 'Oh! Remember to enter the amount in numbers',
+            icon: "error",
+            confirmButtonColor: '#007bff'
+        });   
+        return false;
+      } else if (deposit < 0) {
+        setDeposit(0);
+        MySwal.fire({
+            title: <strong>You got an error!</strong>,
+            html: 'A negative amount cannot be deposited',
+            icon: "error",
+            confirmButtonColor: '#007bff'
+        });
+        return false; 
+      } 
+
+      ctx.users[ctx.loggedIn.index].balance += parseInt(deposit)
+
+      setDeposit(0);
+      MySwal.fire({
+        title: <strong>Success!</strong>,
+        html: 'Your deposit was successful',
+        icon: "success",
+        confirmButtonColor: '#007bff'
+      });
+      return true; 
+    };
+    
     return (
-        <h1>Deposit <br/>
-            {JSON.stringify(ctx)}
-        </h1>
-    );
-}
+        <Card
+        bgcolor = 'light'
+        txtcolor = 'dark'
+        align = 'center'
+        header = 'Withdraw'
+        body = {
+            <>
+            <Stack gap={3} className="col-md-8 mx-auto">
+            <Form.Label>
+                <strong>Balance: ${' '}
+                    {ctx.loggedIn.status
+                        ? ctx.users[ctx.loggedIn.index].balance
+                        : 'Not logged in'
+                    } 
+                </strong>
+            </Form.Label>
+            
+            Transaction amount:
+            
+            <Form.Control type="text" id="amount" placeholder="Amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)} >
+            </Form.Control>
+            
+            <Button variant="primary" type="submit" onClick={handleDeposit} disabled={!deposit ? true: false}>Deposit</Button>
+            </Stack>
+            </>
+            }
+        />
+      );
+    };
--- src/withdraw.js
+++ src/withdraw.js
@@ -1,11 +1,98 @@
 import React from "react";
 import { UserContext } from "./context";
-
+import Card from "./context";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+ 
 export default function Withdraw() {
-    const ctx = React.useContext(UserContext)
+    const {useState, useContext} = React
+    const ctx = useContext(UserContext);
+    const [withdraw, setWithdraw] = useState(0);
+    const MySwal = withReactContent(Swal)
+    
+    const handleWithdraw = () => {
+      if (ctx.loggedIn.status === false) {
+        MySwal.fire({
+            title: <strong>You got an error!</strong>,
+            html: `Please you need to login`,
+            icon: "error",
+            confirmButtonColor: '#007bff'
+        });
+        return false;
+      }
+
+      if (isNaN(parseInt(withdraw))) {
+        setWithdraw(0);
+        MySwal.fire({
+            title: <strong>You got an error!</strong>,
+            html: 'Oh! Remember to enter the amount in numbers',
+            icon: "error",
+            confirmButtonColor: '#007bff'
+        });   
+        return false;
+      } else if ((parseInt(withdraw)) < 0) {
+        setWithdraw(0);
+        MySwal.fire({
+            title: <strong>You got an error!</strong>,
+            html: 'A negative amount cannot be withdrawn',
+            icon: "error",
+            confirmButtonColor: '#007bff'
+        });
+        return false; 
+      } else if (ctx.users[ctx.loggedIn.index].balance < parseInt(withdraw)) {
+        setWithdraw(0);
+        MySwal.fire({
+            title: <strong>You got an error!</strong>,
+            html: 'You do not have the funds to withdraw',
+            icon: "error",
+            confirmButtonColor: '#007bff'
+        });
+        return false; 
+      }
+
+      ctx.users[ctx.loggedIn.index].balance -= parseInt(withdraw)
+
+      setWithdraw(0);
+      MySwal.fire({
+        title: <strong>Success!</strong>,
+        html: 'Your withdrawal was successful',
+        icon: "success",
+        confirmButtonColor: '#007bff'
+      });
+      return true; 
+    };
+    
     return (
-        <h1>Withdraw <br/>
-            {JSON.stringify(ctx)}
-        </h1>
-    );
-}
+        <Card
+        bgcolor = 'light'
+        txtcolor = 'dark'
+        align = 'center'
+        header = 'Deposit'
+        body = {
+            <>
+            <Stack gap={3} className="col-md-8 mx-auto">
+            <Form.Label>
+                <strong>Balance: ${' '}
+                    {ctx.loggedIn.status
+                        ? ctx.users[ctx.loggedIn.index].balance
+                        : 'Not logged in'
+                    } 
+                </strong>
+            </Form.Label>
+
+            Transaction amount:
+
+            <Form.Control type="text" id="amount" placeholder="Amount" value={withdraw} onChange={e => setWithdraw(e.currentTarget.value)} >
+            </Form.Control>
+
+            <Button variant="primary" type="submit" onClick={handleWithdraw} disabled={!withdraw ? true: false}>Withdraw</Button>
+            </Stack>
+            </>
+            }
+        />
+      );
+    };
