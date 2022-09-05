37ace6f352385d7f5bd5970215f26d04e429c82a
Gian <e.dolphinxyz@gmail.com>
2022-09-05 14:26:56 -0500

    Add react-bootstrap/Container

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
 
 export const UserContext = React.createContext(null);
 
@@ -37,14 +38,16 @@ export default function Card(props) {
     }
 
     return(
-        <div className={classes()} style={{maxWidth: '18rem'}}>
-            <div className="card-header">{props.header}</div>
-            <div className="card-body">
-                {props.title && (<h5 className='card-title'>{props.title}</h5>)}
-                {props.text && (<p className='card-text'>{props.text}</p>)}
-                {props.body}
-                {props.status && (<div className='card-text'>{props.status}</div>)}
+        <Container style={{marginTop:'2rem', maxWidth: '20rem'}}>
+            <div className={classes()} style={{maxWidth: '18rem'}}>
+                <div className="card-header">{props.header}</div>
+                <div className="card-body">
+                    {props.title && (<h5 className='card-title'>{props.title}</h5>)}
+                    {props.text && (<p className='card-text'>{props.text}</p>)}
+                    {props.body}
+                    {props.status && (<div className='card-text'>{props.status}</div>)}
+                </div>
             </div>
-        </div>
+        </Container>
     );
 }
