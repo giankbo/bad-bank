74f9ee71c399614fd48a297887dc8ac0deff79bc
Gian <e.dolphinxyz@gmail.com>
2022-09-05 18:09:49 -0500

    Fix card-header and add new style

--- src/context.js
+++ src/context.js
@@ -12,7 +12,7 @@ export const UserContextProvider = (props) => {
                         name:'Abel', 
                         email:'abel@mit.edu', 
                         password:'verysecret', 
-                        balance:100
+                        balance:100 
                     },
                 ], 
                 loggedIn: {
@@ -34,14 +34,14 @@ export default function Card(props) {
         const bg = props.bgcolor ? `bg-${props.bgcolor}` : ' ';
         const txt = props.txtcolor ? `text-${props.txtcolor}` : ' text-white';
         const align = props.align ? `text-${props.align}` : ' ';
-        return  `card mb-3 ${bg} ${txt} ${align}`;
+        return  `card mb-3 ${bg} ${txt} ${align} border border-light rounded-4`;
     }
 
     return(
         <Container style={{marginTop:'2rem', maxWidth: '20rem'}}>
             <div className={classes()} style={{maxWidth: '18rem'}}>
-                <div className="card-header">{props.header}</div>
-                <div className="card-body">
+                <div className="card-header text-center rounded-top" style={{color:'#212529', padding:'1rem'}}><h5><strong>{props.header}</strong></h5></div>
+                <div className="card-body rounded-4" style={{background:'white'}}>
                     {props.title && (<h5 className='card-title'>{props.title}</h5>)}
                     {props.text && (<p className='card-text'>{props.text}</p>)}
                     {props.body}
