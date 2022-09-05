1f4a68945a5ffb929d4989c4e542ed7781e9e411
Gian <e.dolphinxyz@gmail.com>
2022-09-05 15:34:49 -0500

    Fix initial balance

--- src/createaccount.js
+++ src/createaccount.js
@@ -43,7 +43,7 @@ export default function CreateAccount() {
         if(!validate(name, 'name')) return;
         if(!validate(email, 'email')) return;
         if(!validate(password, 'password')) return;
-        ctx.users.push({name, email, password, balance: 100});
+        ctx.users.push({name, email, password, balance: 0});
         setShow(false);
     }
 
