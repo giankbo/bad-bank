71a85b7f92549eca2e1b99813db9699d0236d7b6
Gian <e.dolphinxyz@gmail.com>
2022-09-05 15:20:16 -0500

    Add center align

--- src/home.js
+++ src/home.js
@@ -9,6 +9,7 @@ export default function Home() {
             bgcolor="light"
             txtcolor="dark"
             header="BadBank Landing Page"
+            align = "center"
             title="Welcome to the bank"
             text="You can move around using the navigation bar"
             body={(<img src={bank} className="img-fluid" alt="Responsive bank" />)}
