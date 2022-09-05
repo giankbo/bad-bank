8c7bc0bd4bd3dd8f953559df334dc3672589fe72
Gian <e.dolphinxyz@gmail.com>
2022-09-05 14:05:37 -0500

    Remove Card, add logic and Table component

--- src/alldata.js
+++ src/alldata.js
@@ -1,19 +1,37 @@
 import React from "react";
 import { UserContext } from "./context";
-import Card from "./context";
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 
 export default function AllData() {
     const ctx = React.useContext(UserContext)
     return (
-        <Card
-        bgcolor = 'light'
-        txtcolor = "dark"
-        header = 'AllData'
-        body={
-        <ul>
-            {JSON.stringify(ctx)}
-        </ul>
-        }
-        />
+            <>
+            <Container>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>Name:</th>
+                            <th>Email:</th>
+                            <th>Password:</th>
+                            <th>Balance:</th>
+                        </tr>
+                    </thead>
+                    {ctx.users.map((user, index) => {
+                        return (
+                            <tbody key={index}>
+                                <tr>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.password}</td>
+                                    <td>{user.balance}</td>
+                                </tr>
+                            </tbody>
+                        );
+                    })}
+                </Table>
+            </Container>
+            </>
+
     );
 }
